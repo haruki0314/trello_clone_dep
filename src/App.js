f479805bf6d8ad53caaa397bbabb2bb6a93b2e58
haruki0314 <haruki1472@icloud.com>
2022-09-24 13:27:58 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div style={{ padding: "50px" }}>
+      <div className="side">aaa</div>
+      <h1 style={{ padding: "20px" }}>Trello</h1>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
